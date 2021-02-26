5d625e679322ddda94bb15ec2f596a59b9728ce9
Vikas Chaudhary <42292406+softaim@users.noreply.github.com>
2021-02-26 13:27:53 +0530

    Add files via upload

---
+++ second'/js/footer.js
@@ -0,0 +1,7 @@
+// Write out standard footer and links
+document.write('Copyright &copy; 2021 <a href="http://www.gkaim.com/">Vikash chaudhary</a> All Rights Reserved');
+document.write( '<span class="pull-right">')
+document.write(' &nbsp; <a href="https://gkaim.com/">Home</a>');
+document.write(' &nbsp; <a href="https://gkaim.com/contact-us">Contact Us</a>');
+document.write(' &nbsp; <a href="https://gkaim.com/privacy-policy/">Privacy Policy</a>');
+document.write(' &nbsp; <a href="https://gkaim.com/terms-conditions-disclaimer/">Terms & Conditions</a>');
